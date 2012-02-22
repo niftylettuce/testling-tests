01307191394fb23d6db774266e6145d352beff15
Nick Baugh <niftylettuce@gmail.com>
2012-02-22 03:45:28 -0500

    Converted to json output so we can render up a nice HTML page in the near future

--- input.js
+++ input.js
@@ -2,12 +2,13 @@
 trying out some input attribute detects
 
 curl -u xxx@xxx \
-  -sSNT input.js 'testling.com/?browsers=iexplore/6.0,iexplore/7.0,iexplore/8.0,iexplore/9.0,chrome/4.0,chrome/5.0,chrome/6.0,chrome/7.0,chrome/8.0,chrome/9.0,chrome/10.0,chrome/11.0,chrome/12.0,chrome/13.0,chrome/14.0,chrome/15.0,firefox/4.0,firefox/5.0,firefox/6.0,firefox/7.0,firefox/8.0,opera/10.0,opera/10.5,opera/11.0,opera/11.5,safari/5.0.5,safari/5.1,firefox/nightly,opera/next,chrome/canary'
+  -sSNT input.js 'testling.com/?browsers=iexplore/6.0,iexplore/7.0,iexplore/8.0,iexplore/9.0,chrome/4.0,chrome/5.0,chrome/6.0,chrome/7.0,chrome/8.0,chrome/9.0,chrome/10.0,chrome/11.0,chrome/12.0,chrome/13.0,chrome/14.0,chrome/15.0,firefox/4.0,firefox/5.0,firefox/6.0,firefox/7.0,firefox/8.0,opera/10.0,opera/10.5,opera/11.0,opera/11.5,safari/5.0.5,safari/5.1,firefox/nightly,opera/next,chrome/canary&output=json'
 */
 
 // Inspired by <http://www.miketaylr.com/code/input-type-attr.html>
 
 var test = require('testling');
+var json = require('jsonify');
 var types = [
     'text'
   , 'search'
@@ -47,9 +48,10 @@ var attributes = [
 ];
 
 test('input', function (t) {
+  var output = [];
   for (var q=0; q < types.length; q+=1 ) {
-    t.log('**********     ' + types[q] + '     **********');
     var $input;
+    var not_supported = [];
     try {
       $input = document.createElement('<input type="' + types[q] + '" />');
     } catch(err) {
@@ -58,9 +60,14 @@ test('input', function (t) {
     $input.setAttribute('type', types[q]);
     for (var p=0; p < attributes.length; p+=1 ) {
       if (!(attributes[p] in $input)) {
-        t.log('`' + attributes[p] + '` not supported');
+        not_supported.push(json.stringify('`' + attributes[p] + '` not supported'));
       }
     }
+    output.push({
+      'type': types[q],
+      'not_supported': not_supported
+    });
   }
+  t.log(output);
   t.end();
 });
