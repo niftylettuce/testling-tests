264d9c3ca0bdedbf496dce972e658cb9076abed0
Nick Baugh <niftylettuce@gmail.com>
2012-02-26 23:58:04 -0500

    Pushing for @substack review of output=log err

--- input.js
+++ input.js
@@ -2,7 +2,7 @@
 trying out some input attribute detects
 
 curl -u xxx@xxx \
-  -sSNT input.js 'testling.com/?browsers=iexplore/6.0,iexplore/7.0,iexplore/8.0,iexplore/9.0,chrome/4.0,chrome/5.0,chrome/6.0,chrome/7.0,chrome/8.0,chrome/9.0,chrome/10.0,chrome/11.0,chrome/12.0,chrome/13.0,chrome/14.0,chrome/15.0,firefox/4.0,firefox/5.0,firefox/6.0,firefox/7.0,firefox/8.0,opera/10.0,opera/10.5,opera/11.0,opera/11.5,safari/5.0.5,safari/5.1,firefox/nightly,opera/next,chrome/canary&output=json'
+  -sSNT input.js 'testling.com/?browsers=iexplore/6.0,iexplore/7.0,iexplore/8.0,iexplore/9.0,chrome/4.0,chrome/5.0,chrome/6.0,chrome/7.0,chrome/8.0,chrome/9.0,chrome/10.0,chrome/11.0,chrome/12.0,chrome/13.0,chrome/14.0,chrome/15.0,firefox/4.0,firefox/5.0,firefox/6.0,firefox/7.0,firefox/8.0,opera/10.0,opera/10.5,opera/11.0,opera/11.5,safari/5.0.5,safari/5.1,firefox/nightly,opera/next,chrome/canary&output=log > output.json'
 */
 
 // Inspired by <http://www.miketaylr.com/code/input-type-attr.html>
@@ -60,7 +60,7 @@ test('input', function (t) {
     $input.setAttribute('type', types[q]);
     for (var p=0; p < attributes.length; p+=1 ) {
       if (!(attributes[p] in $input)) {
-        not_supported.push(json.stringify('`' + attributes[p] + '` not supported'));
+        not_supported.push(json.stringify(attributes[p]));
       }
     }
     output.push({
